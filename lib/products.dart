a7105175ba274d0bf2f9b61e4faa2645c62904cc
testUser <shubh_tiwari@hotmail.com>
2020-04-19 16:45:35 +0530

    Implementation of ListView

---
+++ lib/products.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  final List<String> products;
+  Products(this.products);
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/macbook.jpg'),
+          Text(products[index], style: TextStyle(color: Colors.deepPurple))
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
+    );
+  }
+}
